1ef903c1a905801147efd6e6efa3479839840181
Chris Karcz <ckarcz@gmail.com>
2018-09-07 15:59:59 -0400

    Update loggerFactory.js

--- src/common/util/loggerFactory.js
+++ src/common/util/loggerFactory.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 logLevelPrefix.reg(logLevel);
 logLevelPrefix.apply(logLevel, {
-  template: '[DC] [%t] %l (%n):',
+  template: '[EXTENSION] [%t] %l (%n):',
   levelFormatter(level) {
     return level.toUpperCase();
   },
