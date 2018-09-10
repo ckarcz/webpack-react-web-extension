1d2881f0024efd4f3be1611486cfbc45148b596a
Chris Karcz <ckarcz@gmail.com>
2018-09-10 09:10:43 -0400

    Update webpack.config.production.js

--- webpack/webpack.config.production.js
+++ webpack/webpack.config.production.js
@@ -2,8 +2,6 @@
  * Base webpack config common to all build types.
  */
 
-/* eslint-disable no-console */
-
 const merge = require('webpack-merge');
 
 const baseConfigFactory = require('./webpack.config.base');
